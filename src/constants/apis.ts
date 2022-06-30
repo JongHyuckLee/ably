1471ec16c6a3c172c9cca927043fa094d3143bae
JongHyuckLee <yarn add --dev --exact prettier>
2022-06-30 21:01:48 +0900

    routing 추가, api constants 추가

---
+++ src/constants/apis.ts
@@ -0,0 +1,6 @@
+const API_PREFIX = "/api";
+
+export const API_LOGIN = `${API_PREFIX}/login`;
+export const API_LOGOUT = `${API_PREFIX}/logout`;
+export const API_USER_INFO = `${API_PREFIX}/user`;
+export const API_RESET_PASSWORD = `${API_PREFIX}/reset-password`;
